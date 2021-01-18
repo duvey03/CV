708572e1216efb7f0fbdfd034485007b10e072cc
duvey03 <58060878+duvey03@users.noreply.github.com>
2021-01-18 11:35:29 -0700

    Initial upload of CV html code

---
+++ main.py
@@ -0,0 +1,12 @@
+# Browser sends a message to the ISP requesting a certain page.
+# ISP relays the message to a DNS (Domain Name System Server) which is basically a phone book
+# DNS forwards the IP address of the page/server back to the ISP and then back to you.
+
+# Through the ISP, the browser sends a direct request to the service, via the internet backbone of cables and whatnot
+# This message goes to the server with the files on the page, which sends the files back to your browser
+
+# The files that come back from the server will come back as 3 formats: HTML, CSS and JS
+# HTML code controls the structure of the web page...Buttons, images, text boxes, etc
+# CSS code is used to style a web page. These are the colours, fonts, etc
+# Javascript code gives a web-page functionality. It gives buttons functions
+
